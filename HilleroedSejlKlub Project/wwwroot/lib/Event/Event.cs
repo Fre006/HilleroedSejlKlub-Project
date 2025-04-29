29ad35d30fef21d2d0b6552724ea1729a3c188a3
Ahmed Hemdan <aha007@edu.zealand.dk>
2025-04-29 10:31:44 +0200

    aaa

---
+++ HilleroedSejlKlub Project/wwwroot/lib/Event/Event.cs
@@ -0,0 +1,23 @@
+﻿namespace HilleroedSejlKlub_Project.wwwroot.lib.Event
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public List<Member> RegisteredMembers { get; set; }
+
+        public Event(int id, string name, DateTime date)
+        {
+            Id = id;
+            Name = name;
+            Date = date;
+            RegisteredMembers = new List<Member>();
+        }
+
+        public void RegisterMember(Member member)
+        {
+            RegisteredMembers.Add(member);
+        }
+    }
+}
---
+++ HilleroedSejlKlub Project/wwwroot/lib/Member/Member.cs
@@ -0,0 +1,14 @@
+﻿namespace HilleroedSejlKlub_Project.wwwroot.lib
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Member(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
