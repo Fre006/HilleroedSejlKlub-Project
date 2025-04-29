9e116f625521c5ed9fb02499b1af4c4ee5933111
Ahmed Hemdan <aha007@edu.zealand.dk>
2025-04-29 12:39:50 +0200

    aa

---
+++ HilleroedSejlKlub Project/Pages/Events.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HilleroedSejlKlub_Project.Pages
+{
+    public class EventsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
