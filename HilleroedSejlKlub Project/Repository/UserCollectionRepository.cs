c255c8497ce08974e79a0854f9a4ffd63b504071
Frederick Grønbech Kure Ertel <fre006@edu.zealand.dk>
2025-04-29 12:40:37 +0200

    test

--- HilleroedSejlKlub Project/Repository/UserCollectionRepository.cs
+++ HilleroedSejlKlub Project/Repository/UserCollectionRepository.cs
@@ -1,6 +1,8 @@
-﻿namespace HilleroedSejlKlub_Project.Repository
+﻿using HilleroedSejlKlub_Project.Models;
+namespace HilleroedSejlKlub_Project.Repository
 {
     public class UserCollectionRepository
     {
+        protected List<User> _users = new List<User>();
     }
 }
