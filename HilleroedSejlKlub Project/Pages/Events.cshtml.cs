0b1719ed62886797fb40dd63f61f6b9a2040b7fd
Ahmed Hemdan <aha007@edu.zealand.dk>
2025-04-29 20:54:57 +0200

    qq

--- HilleroedSejlKlub Project/Pages/Events.cshtml.cs
+++ HilleroedSejlKlub Project/Pages/Events.cshtml.cs
@@ -1,3 +1,5 @@
+using HilleroedSejlKlub_Project.Models;
+using HilleroedSejlKlub_Project.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,16 @@ namespace HilleroedSejlKlub_Project.Pages
 {
     public class EventsModel : PageModel
     {
+        public List<Event> Events { get; set; }
+
         public void OnGet()
         {
+            EventService service = new EventService();
+
+            service.CreateEvent(new Event(1, "Informationsaften", new DateTime(2025, 4, 1)));
+            service.CreateEvent(new Event(2, "Arbejdsdag", new DateTime(2025, 5, 1)));
+
+            Events = service.GetAllEvents();
         }
     }
 }
