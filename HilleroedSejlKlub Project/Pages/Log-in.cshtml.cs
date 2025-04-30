e9d0ad973dfab1aa33d21edb674362f2a0683a42
Frederick Grønbech Kure Ertel <fre006@edu.zealand.dk>
2025-04-30 22:54:09 +0200

    log-in page with a button to create user

---
+++ HilleroedSejlKlub Project/Pages/Log-in.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HilleroedSejlKlub_Project.Pages
+{
+    public class Log_inModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
