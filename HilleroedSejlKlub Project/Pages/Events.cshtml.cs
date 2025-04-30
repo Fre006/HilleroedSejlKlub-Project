45bfc5e2d50a399e5d5006c5ee2f2314dd56a329
Ahmed Hemdan <aha007@edu.zealand.dk>
2025-04-30 22:28:29 +0200

    dw

--- HilleroedSejlKlub Project/Pages/Events.cshtml.cs
+++ HilleroedSejlKlub Project/Pages/Events.cshtml.cs
@@ -1,39 +1,24 @@
 using HilleroedSejlKlub_Project.Models;
 using HilleroedSejlKlub_Project.Services;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
 namespace HilleroedSejlKlub_Project.Pages
 {
     public class EventsModel : PageModel
     {
         private readonly EventService _eventService;
-        public List<Event> Events { get; set; }
-
-        [BindProperty]
-        public int MemberId { get; set; }
 
         public EventsModel(EventService eventService)
         {
             _eventService = eventService;
         }
 
+        public List<Event> Events { get; set; } = new();
+
         public void OnGet()
         {
             Events = _eventService.GetAllEvents();
-        }
-
-        public IActionResult OnPostRegister(int eventId)
-        {
-            Member member = new Member(MemberId, $"Member {MemberId}");
-            _eventService.RegisterMemberToEvent(eventId, member);
-            return RedirectToPage();
-        }
-
-        public IActionResult OnPostUnregister(int eventId)
-        {
-            _eventService.UnregisterMemberFromEvent(eventId, MemberId);
-            return RedirectToPage();
         }
     }
 }
