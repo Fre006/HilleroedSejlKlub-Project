3a896f39b5c4844f6f82133bb8187e84174c9ca7
Frederick Grønbech Kure Ertel <fre006@edu.zealand.dk>
2025-05-01 10:44:32 +0200

    trying to fix the code and create singleton for user

--- HilleroedSejlKlub Project/Pages/Register.cshtml.cs
+++ HilleroedSejlKlub Project/Pages/Register.cshtml.cs
@@ -1,8 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using HilleroedSejlKlub_Project.Models;
-using HilleroedSejlKlub_Project.Services;
 using System.Collections.Generic;
+using HilleroedSejlKlub_Project.Service;
 
 namespace HilleroedSejlKlub_Project.Pages
 {
